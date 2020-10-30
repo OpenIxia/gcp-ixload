dcf9a8581bc63996a36cb3f68281d94109065888
armdupre <66397339+armdupre@users.noreply.github.com>
2020-10-30 14:54:59 -0400

    Updated.

--- Deployment/Terraform/IxLoad/IxLoad-on-GCP-1-VMone-Demo-Use-Case/variables.tf
+++ Deployment/Terraform/IxLoad/IxLoad-on-GCP-1-VMone-Demo-Use-Case/variables.tf
@@ -41,16 +41,16 @@ variable "GCP_MGMT_FIREWALL_RULE_SOURCE_IP_RANGES" {
 
 variable "GCP_CLIENT_MACHINE_TYPE" {
 	type = string
-	default = "n1-standard-2"
+	default = "n1-standard-4"
 	validation {
-		condition = can(regex("n1-standard-4", var.GCP_CLIENT_MACHINE_TYPE)) || can(regex("n1-standard-2", var.GCP_CLIENT_MACHINE_TYPE))
-		error_message = "GCP_CLIENT_MACHINE_TYPE must be one of (n1-standard-4 | n1-standard-2) types."
+		condition = can(regex("n1-standard-8", var.GCP_CLIENT_MACHINE_TYPE)) || can(regex("n1-standard-4", var.GCP_CLIENT_MACHINE_TYPE))
+		error_message = "GCP_CLIENT_MACHINE_TYPE must be one of (n1-standard-8 | n1-standard-4) types."
 	}
 }
 
 variable "GCP_VMONE_MACHINE_TYPE" {
 	type = string
-	default = "c2-standard-8"
+	default = "c2-standard-16"
 	validation {
 		condition = can(regex("c2-standard-16", var.GCP_VMONE_MACHINE_TYPE)) || can(regex("c2-standard-8", var.GCP_VMONE_MACHINE_TYPE)) || can(regex("c2-standard-4", var.GCP_VMONE_MACHINE_TYPE))
 		error_message = "GCP_VMONE_MACHINE_TYPE must be one of (c2-standard-16 | c2-standard-8 | c2-standard-4) types."
--- Deployment/Terraform/IxVM/IxVM-on-GCP-1-VMone-Demo-Use-Case/variables.tf
+++ Deployment/Terraform/IxVM/IxVM-on-GCP-1-VMone-Demo-Use-Case/variables.tf
@@ -41,7 +41,7 @@ variable "GCP_MGMT_FIREWALL_RULE_SOURCE_IP_RANGES" {
 
 variable "GCP_VMONE_MACHINE_TYPE" {
 	type = string
-	default = "c2-standard-8"
+	default = "c2-standard-16"
 	validation {
 		condition = can(regex("c2-standard-16", var.GCP_VMONE_MACHINE_TYPE)) || can(regex("c2-standard-8", var.GCP_VMONE_MACHINE_TYPE)) || can(regex("c2-standard-4", var.GCP_VMONE_MACHINE_TYPE))
 		error_message = "GCP_VMONE_MACHINE_TYPE must be one of (c2-standard-16 | c2-standard-8 | c2-standard-4) types."
